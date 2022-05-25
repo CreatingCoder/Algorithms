b107e3d5c6b93aafe3a371bb64c3615c8ceb1ac8
Shane <69217993+CreatingCoder@users.noreply.github.com>
2022-05-25 13:06:31 -0500

    updated print statements

--- Insertion/Insertion.py
+++ Insertion/Insertion.py
@@ -4,13 +4,17 @@
 # 
 array = [5,3,4,1,2]
 
+print('we skip over position 0 comparison because there is no value to the left of it')
+
 for i in range(1, len(array)):
     valueToCompare = array[i]
-    print('comparing the value' + valueToCompare)
     position = i
+    print('comparing the value ' + str(valueToCompare) + ' at position ' + str(position))
+    
 
     while(0 < position and array[position -1] > valueToCompare):
         array[position] = array[position - 1]
+        print('comparing ' + str(valueToCompare) + ' with ' + str(array[position]))
         position = position -1
 
     array[position] = valueToCompare
