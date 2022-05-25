b6e480a7901e59e79e2cf1bb0aec826120ebb923
Shane <69217993+CreatingCoder@users.noreply.github.com>
2022-05-25 15:42:06 -0500

    Moved to InsertionSort folder

---
+++ InsertionSort/Insertion.py
@@ -0,0 +1,27 @@
+#Insertion Sort Algorithm
+# Referenced https://www.bigocheatsheet.com/ for complexity times
+#
+#Time complexity (Big O)
+#------------------
+# Best case:   | O(n)
+# Worst case:  | O(n^2)
+# Average case:| O(n^2)
+
+array = [5,3,4,1,2]
+
+print('we skip over position 0 comparison because there is no value to the left of it')
+
+for i in range(1, len(array)):
+    valueToCompare = array[i]
+    position = i
+    print('comparing ' + str(valueToCompare) + ' at position ' + str(position))
+    
+
+    while(0 < position and array[position -1] > valueToCompare):
+        array[position] = array[position - 1]
+        print('moved ' + str(valueToCompare) + ' to the left ' + str(array[position]))
+        position = position -1
+
+    array[position] = valueToCompare
+    
+print('the final result is ' + str(array))
