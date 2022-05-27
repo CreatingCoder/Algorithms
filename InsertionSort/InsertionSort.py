96a7de52eb0581452e8990c7c27352477aa920b9
Shane <69217993+CreatingCoder@users.noreply.github.com>
2022-05-26 20:24:22 -0500

    added def

--- InsertionSort/InsertionSort.py
+++ InsertionSort/InsertionSort.py
@@ -7,21 +7,25 @@
 # Worst case:  | O(n^2)
 # Average case:| O(n^2)
 
-array = [5,3,4,1,2]
+def InsertionSort(array): 
 
-print('we skip over position 0 comparison because there is no value to the left of it')
+    print('we skip over position 0 comparison because there is no value to the left of it')
 
-for i in range(1, len(array)):
-    valueToCompare = array[i]
-    position = i
-    print('comparing ' + str(valueToCompare) + ' at position ' + str(position))
-    
+    for i in range(1, len(array)):
+        valueToCompare = array[i]
+        position = i
+        print('comparing ' + str(valueToCompare) + ' at position ' + str(position))
+        
 
-    while(0 < position and array[position -1] > valueToCompare):
-        array[position] = array[position - 1]
-        print('moved ' + str(valueToCompare) + ' to the left ' + str(array[position]))
-        position = position -1
+        while(0 < position and array[position -1] > valueToCompare):
+            array[position] = array[position - 1]
+            print('moved ' + str(valueToCompare) + ' to the left ' + str(array[position]))
+            position = position -1
 
-    array[position] = valueToCompare
-    
-print('the final result is ' + str(array))
+        array[position] = valueToCompare
+        
+    print('the final result is ' + str(array))
+
+
+myArray = [5,3,4,1,2]
+InsertionSort(myArray)
