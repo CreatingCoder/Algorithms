d3f723a52d5cb9151d7fd094cf2661f47ad8d23d
Shane <69217993+CreatingCoder@users.noreply.github.com>
2022-05-26 20:40:23 -0500

    Updated code

--- InsertionSort/InsertionSort.js
+++ InsertionSort/InsertionSort.js
@@ -12,19 +12,22 @@ function InsertionSort(array) {
   
     
     console.log('we skip over position 0 comparison because there is no value to the left of it');
+    for(let i = 0; i < array.length; i++){
 
-    
-        
-    
-    
-        console.log('the final result is ' );
-      
-        
+        let valueToCompare = array[i];
+        position = i;
+        console.log('Comparing ' + valueToCompare + ' at position ' + position);
 
-    
-   
+        while(0 < position && array[position - 1] > valueToCompare){
+            array[position] = array[position - 1];
+            console.log('moved ' + valueToCompare + ' to the left ' + array[position]);
+            position = position - 1;
+        }
+        array[position] = valueToCompare
+    } 
+    console.log('the final result is ' + array)  
   return array;
 }
 
-array = [5,3,4,1,2];
-InsertionSort(array);
+myArray = [5,3,4,1,2];
+InsertionSort(myArray);
